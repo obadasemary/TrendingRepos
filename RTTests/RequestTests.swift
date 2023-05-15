7e07a78d88c655a826705b94cb0b8b4999b4cb43
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-16 00:19:38 +0300

    Add Request Tests

--- RTTests/RequestTests.swift
+++ RTTests/RequestTests.swift
@@ -5,4 +5,51 @@
 //  Created by Abdelrahman Mohamed on 15.05.2023.
 //
 
-import Foundation
+import XCTest
+@testable import RT
+
+final class RequestTests: XCTestCase {
+
+    var sut: RequestStub!
+
+    override func setUpWithError() throws {
+
+        sut = RequestStub.acceptableEndPoint
+    }
+
+    override func tearDown() async throws {
+
+        sut = nil
+    }
+
+    func test_WhenAcceptableEndPointProvided_ThenUrlNotNil() {
+
+        sut = .acceptableEndPoint
+
+        let request = sut.creatUrlRequest()
+
+        XCTAssertNotNil(request)
+    }
+
+    func test_WhenUnAcceptableEndPointProvided_ThenUrlIsNil() {
+
+        sut = .unAcceptableEndPoint
+
+        let requset = sut.creatUrlRequest()
+
+        XCTAssertNil(requset)
+    }
+
+    func test_WhenUnAcceptableEndPointProvided_ThenCreatUrl() {
+
+        sut = .acceptableEndPoint
+
+        let request = sut.creatUrlRequest()
+        guard let url = request?.url else { return }
+        let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+
+        XCTAssertEqual(urlComponents?.host, Constants.baseUrl.rawValue)
+        XCTAssertEqual(urlComponents?.path, Constants.knownPath.rawValue)
+        XCTAssertEqual(request?.httpMethod, RequestType.get.rawValue)
+    }
+}
