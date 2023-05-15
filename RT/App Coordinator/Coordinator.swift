d7b315ca83e7918e785a341254ab690d1be4eece
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-15 16:24:52 +0300

    Meet SwiftLint rules

--- RT/App Coordinator/Coordinator.swift
+++ RT/App Coordinator/Coordinator.swift
@@ -17,7 +17,7 @@ class MainCoordinator: Coordinator {
 
     var navigationController: UINavigationController
 
-    init(navigationController: UINavigationController) {
+    init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
 
