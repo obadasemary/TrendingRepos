bb22088febec8176355f4849ffa70ef203d4a93c
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-17 00:18:55 +0300

    Add API Constants

---
+++ RT/Network Layer/APIConstants.swift
@@ -0,0 +1,14 @@
+//
+//  APIConstants.swift
+//  RT
+//
+//  Created by Abdelrahman Mohamed on 16.05.2023.
+//
+
+import Foundation
+
+enum APIConstants: String {
+    case baseURL = "api.github.com"
+    case applicationJson = "application/json"
+    case contentType = "Content-Type"
+}
