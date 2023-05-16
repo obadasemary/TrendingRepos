ad7538de424a84175c9d801b6591be5262df1e3d
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-17 00:18:35 +0300

    Add Trending Repos

---
+++ RT/Model/TrendingRepos.swift
@@ -0,0 +1,13 @@
+//
+//  TrendingRepos.swift
+//  RT
+//
+//  Created by Abdelrahman Mohamed on 16.05.2023.
+//
+
+import Foundation
+
+struct TrendingRepos: Decodable {
+
+    let items: [TrendingRepo]
+}
