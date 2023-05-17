5372a154a0fc2e521ac96c8e8a5bb585cc9468ec
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-17 23:59:35 +0300

    Add TrendingRepoViewController+TVDelegate

---
+++ RT/Scenes/TrendingRepo/TrendingRepoViewController+TableViewDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TrendingRepoViewController+TableViewDelegate.swift
+//  RT
+//
+//  Created by Abdelrahman Mohamed on 17.05.2023.
+//
+
+import UIKit
+
+// MARK: - TableViewDelegate
+
+extension TrendingRepoViewController: UITableViewDelegate {}
