6b781f04c9fc4ce62d0c0f29bea85d3a71460ccc
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-17 04:44:43 +0300

    implement TrendingRepo stub

--- RTTests/Stub/TrendingRepoStub.swift
+++ RTTests/Stub/TrendingRepoStub.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+@testable import RT
+
+extension TrendingRepo {
+
+    static let stub: TrendingRepo = .init(
+        name: "Swift",
+        description: "The Swift Programming Language",
+        language: "C++",
+        starCount: 62712,
+        owner: Owner.stub
+    )
+}
