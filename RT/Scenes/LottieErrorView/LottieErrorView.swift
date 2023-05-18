504f0c46152e1ec305c24a884ea03017b885670d
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-19 02:35:41 +0300

    Add Lottie Error View

---
+++ RT/Scenes/LottieErrorView/LottieErrorView.swift
@@ -0,0 +1,28 @@
+//
+//  LottieErrorView.swift
+//  RT
+//
+//  Created by Abdelrahman Mohamed on 19.05.2023.
+//
+
+import UIKit
+
+class LottieErrorView: UIView {
+
+//    @IBOutlet weak var animationView: LottieAnimationView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subitleLabel: UILabel!
+    @IBOutlet weak var retryButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        titleLabel.font = .boldSystemFont(ofSize: 18)
+        subitleLabel.font = .systemFont(ofSize: 19)
+        subitleLabel.textColor = .secondaryLabel
+        retryButton.layer.cornerRadius = 8
+        retryButton.layer.borderWidth = 2
+        retryButton.layer.borderColor = UIColor.systemGreen.cgColor
+    }
+
+}
