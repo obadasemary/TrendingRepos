ef7a8cc16831805882b296ff7105ebc0d0ab6ac1
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-19 01:22:22 +0300

    implement RefreshControl extension

--- RT/Ex/RefreshControl+Ex.swift
+++ RT/Ex/RefreshControl+Ex.swift
@@ -5,4 +5,16 @@
 //  Created by Abdelrahman Mohamed on 19.05.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIRefreshControl {
+
+    func endRefreshing(delay: TimeInterval) {
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            guard let self = self else { return }
+
+            self.endRefreshing()
+        }
+    }
+}
