292fa6185445867f04c0741ebf44e7f4c17319fa
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-05-19 20:37:00 +0300

    implement Settings VM

--- RT/Scenes/Settings/SettingsViewModel.swift
+++ RT/Scenes/Settings/SettingsViewModel.swift
@@ -5,4 +5,27 @@
 //  Created by Abdelrahman Mohamed on 19.05.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol SettingsViewModelType {
+
+    func updateDarkModeSwitch(_ sender: Bool)
+    func updateErrorModeSwitch(_ sender: Bool)
+}
+
+class SettingsViewModel: SettingsViewModelType {
+
+    let defaults = UserDefaults.standard
+
+    func updateDarkModeSwitch(_ sender: Bool) {
+        defaults.set(sender, forKey: "darkModePreference")
+        UIApplication.shared.windows.forEach { window in
+            window.overrideUserInterfaceStyle = sender ? .dark : .light
+        }
+    }
+
+    func updateErrorModeSwitch(_ sender: Bool) {
+        defaults.set(sender, forKey: "errorModePreference")
+        globalbaseURL = sender ? "badUrl" : "api.github.com"
+    }
+}
